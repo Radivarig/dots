249d572c178479bbfcd2aea826255f18912be7e9
Radivarig <reslav.hollos@gmail.com>
2016-03-28 20:49:55 +0000

    (add) generate random dots

--- src/Dots.jsx
+++ src/Dots.jsx
@@ -1,13 +1,29 @@
 var React = require('react')
 
+// var Line = React.createClass({
+//     render: function() {
+//       return (
+
+//       )
+//     }
+// })
+
 var Dots = React.createClass({
-  render: function() {
+  render() {
     return (
-      <div>
-        dot
-      </div>
+      <span>
+        {
+          this.props.circles.map((c, i)=>{
+            return <circle key={i} {...c} />
+          })
+        }
+      </span>
     )
   }
 })
+          // <line x1='25' y1='25' x2='75' y2='75'
+          //   strokeWidth='5'
+          //   stroke='orange'
+          //   />
 
 module.exports = Dots
--- src/DotsViewer.jsx
+++ src/DotsViewer.jsx
@@ -1,10 +1,36 @@
 var React = require('react')
 var Dots = require('./Dots.jsx')
 
+randomInt = function (min, max) {
+  return min + Math.floor(Math.random() * (max - min + 1))
+}
+
 var DotsViewer = React.createClass({
-  render: function() {
+  getInitialState() {
+    var circles = [] //{cx: 4, cy: 10, r: 10, fill: 'mediumorchid', stroke: 'crimson'}
+    var min = 0, max = 500
+    for (var i = 0; i < 100; ++i)
+      circles.push({
+        cx: randomInt(min, max),
+        cy: randomInt(min, max),
+        r: 1,
+        fill: 'blue',
+      })
+
+    return {circles}
+  },
+
+  render() {
     return (
-      <Dots />
+      <div>
+        <svg
+          style={{border: 'solid 1px red'}}
+          height='500'
+          width='500'
+        >
+        {this.state.circles.map((c, i)=><circle key={i} {...c} />)}
+        </svg>
+      </div>
     )
   }
 })
