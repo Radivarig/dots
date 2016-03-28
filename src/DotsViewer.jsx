2157a12068e265832a1bcaeb240ef17ccfd2bcfd
Radivarig <reslav.hollos@gmail.com>
2016-03-28 20:59:04 +0000

    (add) generate random lines

--- src/DotsViewer.jsx
+++ src/DotsViewer.jsx
@@ -7,8 +7,8 @@ randomInt = function (min, max) {
 
 var DotsViewer = React.createClass({
   getInitialState() {
-    var circles = [] //{cx: 4, cy: 10, r: 10, fill: 'mediumorchid', stroke: 'crimson'}
     var min = 0, max = 500
+    var circles = [] //{cx: 4, cy: 10, r: 10, fill: 'mediumorchid', stroke: 'crimson'}
     for (var i = 0; i < 100; ++i)
       circles.push({
         cx: randomInt(min, max),
@@ -17,7 +17,18 @@ var DotsViewer = React.createClass({
         fill: 'blue',
       })
 
-    return {circles}
+    var lines = []
+    for (var i = 0; i < 20; ++i) {
+      var randX1 = circles[randomInt(0, circles.length -1)]
+      var randX2 = circles[randomInt(0, circles.length -1)]
+      lines.push({
+        x1: randX1.cx, y1:randX1.cy,
+        x2: randX2.cx, y2:randX2.cy,
+        strokeWidth:'1',
+        stroke:'green',
+      })
+    }
+    return {circles, lines}
   },
 
   render() {
@@ -29,6 +40,7 @@ var DotsViewer = React.createClass({
           width='500'
         >
         {this.state.circles.map((c, i)=><circle key={i} {...c} />)}
+        {this.state.lines.map((c, i)=><line key={i} {...c} />)}
         </svg>
       </div>
     )
